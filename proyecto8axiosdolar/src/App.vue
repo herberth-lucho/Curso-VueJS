3eb1f58aab5f2596a60a81b5cebaa27a93dd993b
aLeXyZ <h3ealh@gmail.com>
2019-09-29 22:18:11 -0600

    hasta v30 min 26:43

---
+++ proyecto8axiosdolar/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        <router-view/>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ proyecto8axiosdolar/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <v-layout :wrap="true">
+      <v-flex xs12>
+        <v-card>
+          <v-date-picker 
+          v-model="fecha"
+          full-width
+          locale="es-cl"
+          :min="minimo"
+          :max="maximo"
+          ></v-date-picker>
+        </v-card>
+        <v-card color="error" dark>
+          <v-card-text class="display-1 text-xs-center">
+            650 {{fecha}} - {{maximo}}
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+
+  import  { axios } from "axios";
+export default {
+  data() {
+    return {
+      fecha: '',
+      minimo: '1984',
+      maximo: new Date().toISOString().substr(0, 10)
+    }
+  },
+};
+</script>
