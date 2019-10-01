634e3529b189a949da6247829a12111386e25c54
aLeXyZ <h3ealh@gmail.com>
2019-09-30 21:42:26 -0600

    hasta v30

--- proyecto8axiosdolar/src/App.vue
+++ proyecto8axiosdolar/src/App.vue
@@ -2,18 +2,31 @@
   <v-app>
     <v-content>
       <v-container>
-        <router-view/>
+        <router-view />
+
+        <v-dialog v-model="loading.estado" hide-overlay persistent width="300">
+          <v-card color="primary" dark>
+            <v-card-text>
+              {{loading.titulo}}
+              <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+
       </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-
+import { mapState } from "vuex";
 export default {
-  name: 'App',
+  name: "App",
   data: () => ({
     //
   }),
+  computed: {
+    ...mapState(["loading"])
+  }
 };
 </script>
--- proyecto8axiosdolar/src/store.js
+++ proyecto8axiosdolar/src/store.js
@@ -5,10 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    loading:{
+      titulo: '',
+      estado: false
+    }
   },
   mutations: {
-
+    mostrarLoading(state, payload){
+      state.loading.titulo = payload.titulo
+      state.loading.estado = true
+    },
+    ocultarLoading(state){
+      state.loading.estado = false
+    }
   },
   actions: {
 
